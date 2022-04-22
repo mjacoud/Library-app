b3264ba8a5dae14315e2a9b0f8e1ec31e7e5f3f0
mjacoud <m.jacoud01@gmail.com>
2022-04-21 21:38:13 -0300

    v1
    Developing the modal for adding books

---
+++ script.js
@@ -0,0 +1,17 @@
+/***********DOM**** */
+
+const loginBtn = document.querySelector('.login')
+const addBook = document.querySelector('.add-book')
+const colorMode = document.querySelector('.switch')
+
+loginBtn.addEventListener('click', () => {
+  console.log('funcinou')
+})
+
+addBook.addEventListener('click', () => {
+  console.log('funcinou-2')
+})
+
+colorMode.addEventListener('click', () => {
+  console.log('funcinou-3')
+})
