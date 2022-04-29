97653782c9841b9b7c8bcfa42237b5d9255c3242
mjacoud <m.jacoud01@gmail.com>
2022-04-28 23:54:26 -0300

    v3.02
    New Features
    - Side Counter working
    - Delete Card Working
    
    Incoming Features
    - Delete refreshes the side bar
    - Toggle Button read and Not Read

--- script.js
+++ script.js
@@ -5,22 +5,18 @@ const addBook = document.querySelector('.add-book')
 const colorMode = document.querySelector('.switch')
 const modal = document.querySelector('form')
 const modal_close_btn = document.querySelector('.close-btn')
+const modal_checkbox = document.querySelector('.modal-checkbox')
 const incomplete_book = document.querySelector('.incompleted')
 const complete_book = document.querySelector('.completed')
 const totalpages = document.querySelector('.totalpages')
+const totalbooks = document.querySelector('.books_total')
 const book_name = document.querySelector('#book-name')
 const book_author = document.querySelector('#book-author')
 const book_pages = document.querySelector('#book-pages')
 const book_read = document.querySelector('#book-read')
+const grid = document.querySelector('.books-container')
 /**********Library******** */
 const library = []
-
-let got = {
-  book_name: 'Game of Thrones',
-  book_author: 'G.G. R Martin',
-  book_pages: '980',
-  book_read: true
-}
 
 /************LOGIN******* */
 loginBtn.addEventListener('click', () => {
@@ -44,7 +40,8 @@ window.onclick = function (e) {
     modal.style.display = 'none' // closes the modal with clock on window
   }
 }
-/* Add Book */
+
+/* Add Book Button */
 
 modal.addEventListener('submit', e => {
   if (
@@ -61,6 +58,19 @@ modal.addEventListener('submit', e => {
   }
 })
 
+/*****Book Creator**** */
+/*Book Class*/
+class Book {
+  constructor(book_name, book_author, book_pages, book_read) {
+    this.book_name = book_name
+    this.book_author = book_author
+    this.book_pages = book_pages
+    this.book_read = book_read
+  }
+}
+
+/* Add New Book*/
+
 function book_library() {
   const newBook = new Book(
     book_name.value,
@@ -70,10 +80,10 @@ function book_library() {
   )
   library.push(newBook)
   render(newBook)
+  refresher()
 }
 
-const grid = document.querySelector('.books-container')
-
+/*Render*/
 function render(newBook) {
   const b_block = document.createElement('div')
   const b_name = document.createElement('div')
@@ -83,6 +93,7 @@ function render(newBook) {
   const b_remove = document.createElement('button')
 
   b_block.classList.add('add-book')
+  b_block.setAttribute('id', library.indexOf(newBook))
 
   b_name.textContent = newBook['book_name']
   b_name.classList.add('book-title')
@@ -95,27 +106,52 @@ function render(newBook) {
   b_pages.classList.add('book-pages')
   b_block.appendChild(b_pages)
   b_read.classList.add('book-btn')
-  b_read.textContent = newBook['book_read']
-  b_block.appendChild(b_read)
+
+  if (book_read.checked == true) {
+    b_read.textContent = 'Read'
+    b_read.classList.add('read_book')
+    b_block.appendChild(b_read)
+  } else {
+    b_read.textContent = 'Not Read'
+    b_block.appendChild(b_read)
+    b_read.classList.add('not_read_book')
+    console.log(newBook.book_read)
+  }
+
   b_remove.textContent = 'delete'
   b_remove.classList.add('book-btn')
+  b_remove.setAttribute('id', `delete_${book_name.value}`)
   b_block.appendChild(b_remove)
-}
 
-/*****Book Creator**** */
-class Book {
-  constructor(book_name, book_author, book_pages, book_read) {
-    this.book_name = book_name
-    this.book_author = book_author
-    this.book_pages = book_pages
-    this.book_read = book_read
+  if (book_read.checked == true) {
+    read_number++
+  } else {
+    not_read_number++
   }
-}
 
-/*Render*/
+  pages_number += parseInt(book_pages.value)
+
+  b_remove.addEventListener('click', () => {
+    library.splice(library.indexOf(newBook), 1)
+    grid.removeChild(b_block)
+  })
+}
 
 /*****COLOR MODE ******* */
 
 colorMode.addEventListener('click', () => {
   console.log('funcinou-3')
 })
+
+/*******Info Side Bar**** */
+
+function refresher() {
+  totalbooks.textContent = `Total of Books:${parseInt(library.length)}`
+  complete_book.textContent = `Completed Books: ${read_number}`
+  incomplete_book.textContent = `Incompleted Books: ${not_read_number}`
+  totalpages.textContent = `Total Pages Read: ${parseInt(pages_number)}`
+}
+
+var read_number = 0
+var not_read_number = 0
+var pages_number = 0
